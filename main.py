de89960e9c8ffbafed6b166318378a671a135a33
Lucas-850 <lucasrodriguesocp@gmail.com>
2026-02-25 23:21:37 -0300

    adicionando post

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 import uvicorn
-
+from pydantic import BaseModel
 app = FastAPI()
 
+class TestIn(BaseModel):
+    message: str
 
 @app.get("/")
 async def root():
@@ -11,6 +13,10 @@ async def root():
 @app.get("/health")
 async def healt():
     return {"message": "saúde ?"}
+
+@app.post('/test')
+async def tesst(data: TestIn):
+    return {"message": data.message}
 
 if __name__ == "__main__":
     uvicorn.run("main:app")
